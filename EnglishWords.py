dcd0271d1b02eacf2503a20ae2613e56eca5182d
sohantamang <tamangsohan@gmail.com>
2019-10-05 21:44:59 -0700

    updated dict

--- EnglishWords.py
+++ EnglishWords.py
@@ -5,7 +5,7 @@ class EnglishWords:
         self.dict_hash = {}
         with open(filename) as input:
             for _, line in enumerate(input):
-                line = (line.rstrip()).lower()
+                line = line.rstrip()
                 key = line[0]
                 if key in self.dict_hash:
                     self.dict_hash[key].append(line)
